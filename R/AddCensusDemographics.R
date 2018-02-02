de23a862a393ffff0e9f46ea700d84554710b62c
stmacdonell <7674911+stmacdonell@users.noreply.github.com>
2018-02-02 11:59:01 -0600

    organizing folders

--- R/AddCensusDemographics.R
+++ R/AddCensusDemographics.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,18 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+
+AddCensusDemographics <- function(map,State,County=NULL){
+  library(sf)
+
+
+  if(State%in%)
+
+
+  if(is.null(County)){
+
+  }else{
+
+  }
+
 }
