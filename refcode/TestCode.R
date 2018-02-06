7df60fb763152e19d0cf80b93ef8bd4fc5d478cd
stmacdonell <7674911+stmacdonell@users.noreply.github.com>
2018-02-05 20:47:13 -0600

    Updated code to test function

--- refcode/TestCode.R
+++ refcode/TestCode.R
@@ -3,7 +3,7 @@ source("R/AddCensusDemographics.R")
 api_key <-"d9174b06d5ec03a39d5f89f411614913e72556e6"
 map <- st_read("testing/data/raw/ArlingtonMA/ArlingtonMA_VoterPrecinct.shp")
 state <- 25
-county <- c(17)
+county <- 17
 
 MapWithDemographics <- AddCensusDemographics(map,api_key,state,county)
 
