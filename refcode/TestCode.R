a96eb9400c497bd436b4da47a14996ae9f98ab01
stmacdonell <7674911+stmacdonell@users.noreply.github.com>
2018-03-15 14:45:35 -0400

    Moved Shiny app to it's own folder and merged READMEs

--- refcode/TestCode.R
+++ refcode/TestCode.R
@@ -1,10 +1,27 @@
 source("R/AddCensusDemographics.R")
 
+#set up directories we'll need
+dir.create("data/cache/")
+dir.create("data/cache/ArlingtonMA/")
+dir.create("data/test")
+
+#set api_key
 api_key <-"d9174b06d5ec03a39d5f89f411614913e72556e6"
-map <- st_read("testing/data/raw/ArlingtonMA/ArlingtonMA_VoterPrecinct.shp")
+
+#read the map
+unzip("shiny/TestingData/ArlingtonMA_VoterPrecinct.zip",exdir = "data/cache/ArlingtonMA/")
+map <- st_read("data/cache/ArlingtonMA/ArlingtonMA_VoterPrecinct.shp")
+
+#set parameters
 state <- 25
 county <- 17
 
+#add census demographics
 MapWithDemographics <- AddCensusDemographics(map,api_key,state,county)
 
-st_write(MapWithDemographics,dsn="testing/data/output/",layer="MapWithDemographics",driver = "ESRI Shapefile")
+#output map
+st_write(MapWithDemographics,dsn="data/test/",layer="MapWithDemographics",driver = "ESRI Shapefile")
+
+#clean up directory
+unlink("data/cache",recursive = TRUE)
+unlink("data/test",recursive = TRUE)
